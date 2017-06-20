6292928ce93fdb19fa2509f11ca99d4aa652bb6c
RaphC <rcr@orange.fr>
2017-06-20 02:29:37 +0200

    [JENKINS#21244] surround source arg by single quote to prevent launcher $ char misinterpretation

--- src/main/java/org/jenkinsci/plugins/deploy/weblogic/deployer/WebLogicDeployer.java
+++ src/main/java/org/jenkinsci/plugins/deploy/weblogic/deployer/WebLogicDeployer.java
@@ -70,7 +70,7 @@ public class WebLogicDeployer {
         if(StringUtils.isNotBlank(parameter.getSource())) {
         	args.add("-source");
         	//args.add(parameter.getSource());
-            args.add(new File(parameter.getSource()).getAbsolutePath());
+            args.add("\'"+new File(parameter.getSource()).getAbsolutePath()+"\'");
         }
 
         args.add("-targets");
