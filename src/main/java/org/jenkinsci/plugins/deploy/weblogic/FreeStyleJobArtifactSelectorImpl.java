5aae97fd7c5e361a31594d66eab2c309956b818b
rachaumi <rachaumi@bouyguestelecom.fr>
2017-06-27 00:36:29 +0200

    [JENKINS-36215] Use of FilePath instead of File

--- src/main/java/org/jenkinsci/plugins/deploy/weblogic/FreeStyleJobArtifactSelectorImpl.java
+++ src/main/java/org/jenkinsci/plugins/deploy/weblogic/FreeStyleJobArtifactSelectorImpl.java
@@ -5,8 +5,10 @@ package org.jenkinsci.plugins.deploy.weblogic;
 
 import hudson.EnvVars;
 import hudson.FilePath;
+import hudson.FilePath.FileCallable;
 import hudson.model.AbstractBuild;
 import hudson.model.BuildListener;
+import hudson.remoting.VirtualChannel;
 
 import java.io.File;
 import java.io.IOException;
@@ -15,10 +17,12 @@ import java.util.Collection;
 import java.util.List;
 import java.util.regex.Pattern;
 
-import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang.StringUtils;
 import org.jenkinsci.plugins.deploy.weblogic.util.VarUtils;
+import org.jenkinsci.remoting.RoleChecker;
+
+import com.google.common.collect.Lists;
 
 /**
  * @author rchaumie
@@ -38,37 +42,37 @@ public class FreeStyleJobArtifactSelectorImpl implements ArtifactSelector {
         List<FilePath> artifactsRecorded = new ArrayList<FilePath>();
         
         // On parcours le workspace si aucun repertoire de base specifie a la recherche d'un fichier correspondant a l'expression reguliere
-        Collection<?> filesToCheck = CollectionUtils.EMPTY_COLLECTION;
-        String baseDirName = "";
+        List<FilePath> filesToCheck = Lists.newArrayList();
+        FilePath baseDir = null;
         if(StringUtils.isBlank(baseDirectory)){
-	        FilePath workspace = build.getWorkspace();
-            baseDirName = workspace.getName();
-            filesToCheck = FileUtils.listFiles(new File(workspace.toURI()), null, true);
+        	baseDir = build.getWorkspace();
+            listener.getLogger().println("[WeblogicDeploymentPlugin] - No the base directory set. Looking up for workspace directory located on '"+baseDir+"'...");
         } else {
 
             //Recuperation des variables
             EnvVars vars = VarUtils.getEnvVars(build, listener);
             String resolvedBaseDirectory = vars.expand(baseDirectory);
 
-            File baseDir = new File(resolvedBaseDirectory);
-
+            baseDir = new FilePath(build.getBuiltOn().getChannel(), resolvedBaseDirectory);
+            
         	//si un repertoire est specifie mais qu'il est inacessible ou invalide on renvoit une erreur
-            if(! baseDir.exists() || ! baseDir.isDirectory() || ! baseDir.canRead()){
-            	listener.getLogger().println("[WeblogicDeploymentPlugin] - the base directory specified ["+resolvedBaseDirectory+"] is invalid (doesn't exists or is not a directory or has insufficient privilege). Please check the job configuration");
-            	throw new RuntimeException("The base directory specified ["+resolvedBaseDirectory+"] is invalid (doesn't exists or is not a directory or has insufficient privilege)");
+            if(! baseDir.exists() || ! baseDir.isDirectory() || ! baseDir.toVirtualFile().canRead()){
+            	listener.getLogger().println("[WeblogicDeploymentPlugin] - the following base directory '"+resolvedBaseDirectory+"' is invalid on node '"+build.getBuiltOnStr()+"' (doesn't exist or is not a directory or has insufficient privilege). Please check the job configuration");
+            	throw new RuntimeException("The base directory '"+resolvedBaseDirectory+"' is invalid on node '"+build.getBuiltOnStr()+"' (doesn't exist or is not a directory or has insufficient privilege)");
             }
+        }
+        
+        // lookup files found
+        filesToCheck = baseDir.act(new RecursiveWorkspaceLookup());
 
-            baseDirName = baseDir.getName();
-            filesToCheck = FileUtils.listFiles(baseDir, null, true);
-         }
-
-        listener.getLogger().println("[WeblogicDeploymentPlugin] - "+filesToCheck.size() +" files found under "+baseDirName);
-        for(File file : (Collection<File>) filesToCheck){
+        listener.getLogger().println("[WeblogicDeploymentPlugin] - "+filesToCheck.size() +" files found under directory "+baseDir.getRemote());
+        // TODO : Pourquoi ne pas l'incorporer au list(...) directement ?
+        for(FilePath file : filesToCheck){
             if(! file.isDirectory() && Pattern.matches(filteredResource, file.getName())){
-                listener.getLogger().println("[WeblogicDeploymentPlugin] - the following resource recorded "+file.getAbsolutePath()+" is eligible.");
-                artifactsRecorded.add(new FilePath(file));
+                listener.getLogger().println("[WeblogicDeploymentPlugin] - the following resource recorded "+file+" is eligible.");
+                artifactsRecorded.add(file);
             } else {
-                listener.getLogger().println("[WeblogicDeploymentPlugin] - the following resource ['"+file.getName()+"'] doesn't match "+filteredResource);
+                listener.getLogger().println("[WeblogicDeploymentPlugin] - the following resource ['"+file.getRemote()+"'] doesn't match "+filteredResource);
             }
         }
         
@@ -98,5 +102,24 @@ public class FreeStyleJobArtifactSelectorImpl implements ArtifactSelector {
 	public String getName() {
 		return "FreeStyleProject";
 	}
+	
+	private static class RecursiveWorkspaceLookup implements FileCallable<List<FilePath>> {
+		private static final long serialVersionUID = 1L;
+		
+		@Override
+		public void checkRoles(RoleChecker checker) throws SecurityException {
+		}
+
+		@Override
+		public List<FilePath> invoke(File f, VirtualChannel channel) throws IOException, InterruptedException {
+			List<FilePath> result = Lists.newArrayList();
+			Collection<File> files = FileUtils.listFiles(f, null, true);
+			for(File file : files){
+				result.add(new FilePath(file));
+			}
+			return result;
+		}
+        
+    }
 
 }
